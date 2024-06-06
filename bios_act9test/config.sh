3a71bd4c81257eba18a9c1eaa68dca0e09675593
Sean Bryan <sean.bryan@anu.edu.au>
2024-06-06 11:08:41 +1000

    Move inputs into rp23

--- bios_act9test/config.sh
+++ bios_act9test/config.sh
@@ -1,7 +1,7 @@
 EXE='/home/189/sb8430/cable/offline/cable-mpi'
 INPUT=(
-    '/home/189/sb8430/cable/params/def_veg_params.txt'
-    '/home/189/sb8430/cable/params/def_soil_params.txt'
-    '/home/189/sb8430/cable/params/pftlookup.csv'
+    '/g/data/rp23/experiments/2024-04-17_BIOS3-merge/sb8430/params_bios/def_soil_params.txt'
+    '/g/data/rp23/experiments/2024-04-17_BIOS3-merge/sb8430/params_bios/def_veg_params.txt'
+    '/g/data/rp23/experiments/2024-04-17_BIOS3-merge/sb8430/params_bios/pftlookup.csv'
 )
 RESTART='/g/data/rp23/experiments/2024-04-17_BIOS3-merge/sb8430/bios_act9test/restart_1700_1899'
